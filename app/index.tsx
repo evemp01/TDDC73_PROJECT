e794ae2799ff1a4dfaee7d87c3ef8ee40223e36d
evemp01 <bergstrom.ella1@gmail.com>
2026-01-05 13:44:49 +0100

    Cleaned index file

--- app/index.tsx
+++ app/index.tsx
@@ -1,13 +1,11 @@
 import { RegistrationField } from "@/components/AccountRegistration/types";
 import { PasswordRule } from "@/components/PasswordStrengthMeter/types";
-import { useState } from "react";
-import { Text, TextInput, View } from "react-native";
+import { Text, View } from "react-native";
 import { AccountRegistration } from "../components/AccountRegistration/AccountRegistration";
-import { PasswordStrengthMeter } from "../components/PasswordStrengthMeter/PasswordStrengthMeter";
 
 const fields: RegistrationField[] = [
   { id: "email1", label: "Email", type: "text" },
-  { id: "dob1", label: "Date of Birth", placeholder: "ÅÅÅÅ-MM-DD", type: "date" },
+  { id: "dob1", label: "Date of Birth", placeholder: "ÅÅÅÅ-MM-DD", type: "date"},
   { id: "name1", label: "Name", placeholder: "Enter your name", type: "text" },
   { id: "p1", label: "Password", type: "password" },
 ];
@@ -18,25 +16,9 @@ const rules: PasswordRule[] = [
 ];
 
 export default function Index() {
-  const [password, setPassword] = useState("");
-
   return (
     <View style={{ padding: 20 }}>
-      <Text>Password Strength Meter:</Text>
-      <TextInput
-        placeholder="Password"
-        secureTextEntry={true}
-        value={password}
-        onChangeText={setPassword}
-        style={{
-          borderWidth: 1,
-          padding: 10,
-          marginBottom: 8,
-        }}
-      />
-      <PasswordStrengthMeter password={password} rules={rules} />
       <Text style={{ marginTop: 20 }}>Account Registration:</Text>
-
       <AccountRegistration
         fields={fields}
         passwordRules={rules}
