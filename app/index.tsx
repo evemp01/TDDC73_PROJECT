8a5fbaac2663dae910fee04f51754ef0c7ff8426
Emil Djurson <emidj236@student.liu.se>
2026-01-05 09:28:32 +0100

    Small changes

--- app/index.tsx
+++ app/index.tsx
@@ -1,14 +1,24 @@
+import { RegistrationField } from "@/components/AccountRegistration/types";
+import { PasswordRule } from "@/components/PasswordStrengthMeter/types";
 import { useState } from "react";
 import { Text, TextInput, View } from "react-native";
 import { AccountRegistration } from "../components/AccountRegistration/AccountRegistration";
 import { PasswordStrengthMeter } from "../components/PasswordStrengthMeter/PasswordStrengthMeter";
 
+const fields: RegistrationField[] = [
+  { id: "email1", label: "Email", type: "text" },
+  { id: "dob1", label: "Date of Birth", placeholder: "ÅÅÅÅ-MM-DD", type: "date" },
+  { id: "name1", label: "Name", placeholder: "Enter your name", type: "text" },
+  { id: "p1", label: "Password", type: "password" },
+];
+
+const rules: PasswordRule[] = [
+  { id: "1", label: "Minst 8 tecken", test: (p: string) => p.length >= 8 },
+  { id: "2", label: "Minst en siffra", test: (p: string) => /\d/.test(p) },
+];
+
 export default function Index() {
   const [password, setPassword] = useState("");
-  const myRules = [
-    { id: "1", label: "Minst 8 tecken", test: (p: string) => p.length >= 8 },
-    { id: "2", label: "Minst en siffra", test: (p: string) => /\d/.test(p) },
-  ];
 
   return (
     <View style={{ padding: 20 }}>
@@ -24,31 +34,12 @@ export default function Index() {
           marginBottom: 8,
         }}
       />
-      <PasswordStrengthMeter
-        password={password}
-        rules={[
-          {
-            id: "length",
-            label: "Have at least 6 characters",
-            test: (pw) => pw.length >= 6,
-          },
-          {
-            id: "number",
-            label: "Include at least 1 number",
-            test: (pw) => /\d/.test(pw),
-          },
-        ]}
-      />
+      <PasswordStrengthMeter password={password} rules={rules} />
       <Text style={{ marginTop: 20 }}>Account Registration:</Text>
 
       <AccountRegistration
-        fields={[
-          { id: "email1", label: "Email", type: "text" },
-          { id: "dob1", label: "Date of Birth", placeholder: "ÅÅÅÅ-MM-DD", type: "date" },
-          { id: "name1", label: "Name", placeholder: "Enter your name", type: "text" },
-          { id: "p1", label: "Password", type: "password" },
-        ]}
-        passwordRules={myRules}
+        fields={fields}
+        passwordRules={rules}
         onSubmit={(values) => console.log(values)}
       />
     </View>
